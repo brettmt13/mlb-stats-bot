d7fad10d9157c1c3d71b13485305d93a4ae1b016
brettmt13 <brettmt13@gmail.com>
2023-08-03 23:33:11 -0400

    repo restructure

---
+++ bot/query_strings.py
@@ -0,0 +1,25 @@
+import urllib.parse
+from datetime import date
+from datetime import timedelta
+
+today = date.today()
+
+# yesterday
+game_dt = str(today - timedelta(days = 1))
+
+PARAMS_BBE = {'hfSea': urllib.parse.unquote('2023%7C'), 'hfGT': urllib.parse.unquote('R%7C'), 'chk_game_date_gt': 'on', 'chk_stats_bip': "on", 'game_date_gt': game_dt, 'game_date_lt': game_dt, 'sort_by': 'desc', 'player_type': 'batter'}
+PARAMS_BBDIST = {'hfSea': urllib.parse.unquote('2023%7C'), 'hfGT': urllib.parse.unquote('R%7C'), 'chk_game_date_gt': 'on', 'chk_stats_bbdist': "on", 'game_date_gt': game_dt, 'game_date_lt': game_dt, 'sort_by': 'desc', 'player_type': 'batter'}
+PARAMS_SWING_AND_MISS = {'hfSea': urllib.parse.unquote('2023%7C'), 'hfGT': urllib.parse.unquote('R%7C'), 'chk_game_date_gt': 'on', 'chk_game_date_lt': 'on', 'chk_pitch_result': "on", 'game_date_gt': game_dt, 'game_date_lt': game_dt, 'sort_by': 'desc', 'player_type': 'pitcher', 'hfPR': urllib.parse.unquote('called%5C.%5C.strike%7Cswinging%5C.%5C.pitchout%7Cswinging%5C.%5C.strike%7Cswinging%5C.%5C.strike%5C.%5C.blocked%7C')}
+PARAMS_OBP = {'hfSea': urllib.parse.unquote('2023%7C'), 'hfGT': urllib.parse.unquote('R%7C'), 'chk_game_date_gt': 'on', 'chk_stats_obp': "on", 'game_date_gt': game_dt, 'game_date_lt': game_dt, 'sort_by': 'desc', 'player_type': 'batter'}
+PARAMS_SLG = {'hfSea': urllib.parse.unquote('2023%7C'), 'hfGT': urllib.parse.unquote('R%7C'), 'chk_game_date_gt': 'on', 'chk_stats_slg': "on", 'game_date_gt': game_dt, 'game_date_lt': game_dt, 'sort_by': 'desc', 'player_type': 'batter'}
+PARAMS_EV = {'hfSea': urllib.parse.unquote('2023%7C'), 'hfGT': urllib.parse.unquote('R%7C'), 'chk_game_date_gt': 'on', 'chk_stats_launch_speed': "on", 'game_date_gt': game_dt, 'game_date_lt': game_dt, 'sort_by': 'desc', 'player_type': 'batter'}
+PARAMS_PLATE_Z = {'hfSea': urllib.parse.unquote('2023%7C'), 'hfGT': urllib.parse.unquote('R%7C'), 'chk_game_date_gt': 'on', 'chk_stats_plate_z': "on", 'game_date_gt': game_dt, 'game_date_lt': game_dt, 'sort_by': 'desc', 'player_type': 'pitcher', 'min_pitches': '10'}
+
+# chk_stats_plate_z=on
+query_strings = [PARAMS_BBE,
+                 PARAMS_BBDIST,
+                 PARAMS_SWING_AND_MISS,
+                 PARAMS_OBP,
+                 PARAMS_SLG,
+                 PARAMS_EV,
+                 PARAMS_PLATE_Z]
---
+++ bot/scraper.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import query_strings
+
+'''
+Returns the scraped search results
+
+Params:
+query_params: params for search query
+top_k: top k players of result
+
+Returns:
+dataframe of top k player stats of query
+'''
+def get_data(query_params, top_k=5):
+    res = requests.get("https://baseballsavant.mlb.com/statcast_search", query_params)
+    print("Request URL: ", res.url, "\nStatus: ", res.status_code)
+    soup = BeautifulSoup(res.text, 'lxml')
+
+    search_results_table = soup.find('table', id='search_results')
+
+    columns = search_results_table.find_all('th')
+    headers=[]
+
+    col_ind = 0
+    for col in columns:
+        # indicate this is an image
+        if col_ind == 1:
+            headers.append("Player Image URL")
+        else:
+            headers.append(col.text.strip())
+        col_ind+=1
+
+    results = pd.DataFrame(columns = headers[:-1])
+    
+    rows = search_results_table.find_all('tr', {"data-group-by": "name"})
+    for row in rows:
+        row_items = row.find_all('td')
+        player_img_url = row.find_all('img', {"class": "player-mug"})[0]['src']
+        player_data = [item.text.strip() for item in row_items][:-1]
+        player_data[1] = player_img_url
+        results.loc[len(results)] = player_data
+        
+    if query_params['player_type'] == 'pitcher' and query_params.get('min_pitches') == None: # pitcher columns behave differently
+        results = results[results['Total'] != '']
+        results['Total'] = results['Total'].astype(float)
+        results = results.sort_values('Total', 0, ascending=False)[0:top_k]
+    else:  
+        results = results[results[results.columns[-1]] != '']
+        results[results.columns[-1]] = results[results.columns[-1]].astype(float)
+        results = results.sort_values(results.columns[-1], 0, ascending=False)[0:top_k]
+        
+    if query_params['player_type'] == 'pitcher' and query_params.get('min_pitches') == None:
+        tweet_string = results.to_string(columns=["Player", "Total"], index=False, header=False)
+    else:  
+        tweet_string = results.to_string(columns=["Player", results.columns[-1]], index=False, header=False)
+
+    return tweet_string
