48172f35eae64217833ef5d209b8ec0fa4878b87
brettmt13 <brettmt13@gmail.com>
2023-08-05 21:08:12 -0400

    tweet with image and corresponding name

--- bot/tweeter.py
+++ bot/tweeter.py
@@ -5,6 +5,8 @@ import scraper
 import random
 import yaml
 import os
+import image_gen
+import numpy
 # import image_gen
 
 def pick_three():
@@ -27,7 +29,6 @@ auth.set_access_token(access_token, access_token_secret)
 
 # for media (future release)
 api_v1 = tweepy.API(auth)
-# media_upload = api_v1.media_upload(filename="./image.jpg")
 
 # for text
 api_v2 = tweepy.Client(bearer_token=bearer_token,
@@ -41,15 +42,45 @@ tweet_strings_ = tweet_strings.tweet_strings
 all_inds = list(range(len(query_strings_)))
 choice_inds = pick_three()
 
-for i in range(3):
-    try:
-        data_string, _ = scraper.get_data(query_strings_[choice_inds[i]])
-    except AttributeError:
-        print("Data not yet available for parsing. Try again soon.")
-        exit(1)
-    data_string = data_string.rstrip()
-    data_string = data_string.lstrip()
-    print(data_string)
-    tweet_text = str(query_strings.game_dt + "\n\n" + tweet_strings_[choice_inds[i]] + "\n\n" + data_string)
-    print(tweet_text)
-    api_v2.create_tweet(text=tweet_text)
+
+def gen_tweet_with_text():
+    for i in range(1):
+        try:
+            player_images_data = scraper.get_data_image(query_strings_[choice_inds[i]])
+        except AttributeError:
+            print("Data not yet available for parsing. Try again soon.")
+            exit(1)
+        data_string = data_string.rstrip()
+        data_string = data_string.lstrip()
+        player_images_URL = player_images_data["Player Image URL"]
+        player_names = player_images_data["Player"]
+        # print(data_string)
+        tweet_text = str(query_strings.game_dt + "\n\n" + tweet_strings_[choice_inds[i]] + "\n\n" + data_string)
+        # print(tweet_text)
+        # api_v2.create_tweet(text=tweet_text)
+        print()
+        
+def gen_tweet_only_image():
+    for i in range(1):
+        try:
+            player_images_data = scraper.get_data_image(query_strings_[choice_inds[i]])
+        except AttributeError:
+            print("Data not yet available for parsing. Try again soon.")
+            exit(1)
+        player_images_URL = player_images_data["Player Image URL"].to_numpy()
+        player_names = player_images_data["Player"].to_numpy()
+        print("Checking player names and URL separate...:\n")
+        print(player_images_URL)
+        print(player_names)
+        tweet_text = str(query_strings.game_dt + "\n\n" + tweet_strings_[choice_inds[i]] + "\n\n")
+        image_gen.generate_image(player_images_URL, player_names)
+        media_upload = api_v1.media_upload(filename="twit_img.jpg")
+        api_v2.create_tweet(text=tweet_text, media_ids=[media_upload.media_id])
+
+    # clear images
+    for i in range(5):
+        os.remove(f'player_image{i}.png')
+    os.remove('twit_img.jpg')
+        
+if __name__ == "__main__":
+    gen_tweet_only_image()
